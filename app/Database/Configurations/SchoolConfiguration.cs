85016b39342301a25865943ac61b4eb8aa3a4c8e
Ricardo Milan <ricardo@fluencypass.com>
2022-06-24 08:46:15 -0300

    implementando MSSQLServer

---
+++ app/Database/Configurations/SchoolConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using app.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace app.Database.Configurations
+{
+    public class SchoolConfiguration : IEntityTypeConfiguration<School>
+    {
+        public void Configure(EntityTypeBuilder<School> builder)
+        {
+            builder.HasQueryFilter(s => s.IsActive);
+        }
+    }
+}
