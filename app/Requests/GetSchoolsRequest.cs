6c40aed19a60d416ed7dffc17baddc3860448738
Ricardo Milan <ricardo@fluencypass.com>
2022-06-24 08:27:32 -0300

    optional params

--- app/Requests/GetSchoolsRequest.cs
+++ app/Requests/GetSchoolsRequest.cs
@@ -2,14 +2,17 @@
 {
     public class GetSchoolsRequest
     {
-        public string Type { get; set; } // programa
-        public string Language { get; set; }
+        public string? Type { get; set; } // Idiomas/Formação
+        public string? Language { get; set; }
 
-        public string CourseTitle { get; set; }
-        public int CountryId { get; set; }
-        public int CityId { get; set; }
+        public string? CourseTitle { get; set; }
+        public int? CountryId { get; set; }
+        public int? CityId { get; set; }
 
-        public int WeekQuantity { get; set; }
-        public DateTime StartDate { get; set; }
+        public string AccommodationType { get; set; }
+        public string AccommodationBedroomType { get; set; }
+
+        public int? WeekQuantity { get; set; }
+        public DateTime? StartDate { get; set; }
     }
 }
