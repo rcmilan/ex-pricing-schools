b789fb67dae3c8e6b60ea65578d20909b4813fb1
Ricardo Milan <ricardo@fluencypass.com>
2022-06-25 12:18:36 -0300

    adicionando testes de api

---
+++ app.tests/Controllers/SchoolsController.Tests.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using app.Requests;
+
+namespace app.tests.Controllers
+{
+    internal class SchoolsController
+    {
+        private const string BASE_PATH = "/api/Schools";
+
+        [Test]
+        public async Task ShouldAddSchools()
+        {
+            // Arrange
+            await using var application = new WebAppFactory("Development");
+            var client = application.CreateClient();
+
+            IEnumerable<AddSchool> schools = new List<AddSchool>
+            {
+                new AddSchool
+                {
+                    FantasyName = "",
+                    LegalName = "",
+                    Courses = new List<AddCourse>
+                    {
+                        new AddCourse
+                        {
+                            BookChargedBefore = false,
+                            RegistrationFee = 0,
+                            Title = "",
+                            PriceRanges = new List<AddCoursePriceRange>
+                            {
+                                new AddCoursePriceRange
+                                {
+                                    Price = 0,
+                                    RangeFrom = 0,
+                                    RangeTo = 0
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var byteContent = schools.BuildByteContent();
+
+            // Act
+            var response = await client.PostAsync(BASE_PATH, byteContent, CancellationToken.None);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ app.tests/WebAppFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+namespace app.tests
+{
+    internal class WebAppFactory : WebApplicationFactory<Program>
+    {
+        private readonly string _environment;
+
+        public WebAppFactory(string environment = "Development")
+        {
+            _environment = environment;
+        }
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.UseEnvironment(_environment);
+
+            return base.CreateHost(builder);
+        }
+    }
+}
